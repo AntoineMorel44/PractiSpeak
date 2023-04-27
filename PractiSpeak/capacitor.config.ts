0f0fe596bb83498b9d7317f21e85a7057db295a0
Antoine Morel <antoinemorel2@hotmail.fr>
2023-04-27 19:36:54 +0200

    Adapt to deployment

---
+++ PractiSpeak/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.fexilo.app',
+  appName: 'Fexilo',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
--- PractiSpeak/src/app/app.module.ts
+++ PractiSpeak/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './components/app/app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
 
 @NgModule({
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    IonicModule.forRoot()
   ],
   declarations: [
     AppComponent
